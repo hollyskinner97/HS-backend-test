b0e19a71c211e415d73d26f7a26f1de74f7ab42e
Holly Skinner <hollyskinner97@gmail.com>
2025-04-30 15:41:03 +0100

    import delivery module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DeliveryModule } from './comms/delivery.module';
 
 @Module({
-  imports: [],
+  imports: [DeliveryModule],
   controllers: [AppController],
   providers: [AppService],
 })
